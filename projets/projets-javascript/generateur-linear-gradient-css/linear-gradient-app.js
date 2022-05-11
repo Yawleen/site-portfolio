68f524d2fd4fa5c6acf208adda07fd252bd67fe8
Yolene Ctb <yoleneconstable@live.fr>
2022-05-11 15:08:29 +0200

    Modification app linear gradient

--- projets/projets-javascript/generateur-linear-gradient-css/linear-gradient-app.js
+++ projets/projets-javascript/generateur-linear-gradient-css/linear-gradient-app.js
@@ -16,7 +16,7 @@ const btnCssCode = document.querySelector(".code-css");
 
 // Init
 
-let valCouleurs = { 1: "#b06817", 2: "#eddb93" };
+let valCouleurs = { 1: "#B06817", 2: "#EDDB93" };
 let index = 3;
 let inclinaison = 0;
 
@@ -38,7 +38,7 @@ val.innerHTML = `<span class="linear">linear-gradient</span>(${inclinaison}deg, 
 
 inputsCouleur.forEach((input) => {
   input.addEventListener("input", (e) => {
-    ajoutCouleur(e);
+    ajout_ModifCouleur(e);
   });
 });
 
@@ -65,41 +65,36 @@ function verifNbCouleurs() {
   }
 }
 
-
 function suppCouleur(input) {
-    if (
-        Object.keys(valCouleurs).includes(
-          input.getAttribute("data-index")
-        ) &&
-        Object.keys(valCouleurs).length < 3
-      ) {
-        delete valCouleurs[input.getAttribute("data-index")];
-        document.body.style.background = "#333";
-        comment.textContent =
-          "/* Il faut au moins deux couleurs pour générer un dégradé linéaire et son code CSS. */";
-        prop.textContent = "";
-        ponct.textContent = "";
-        val.innerHTML = "";
-      } else if (
-        Object.keys(valCouleurs).includes(
-          input.getAttribute("data-index")
-        ) &&
-        Object.keys(valCouleurs).length > 2
-      ) {
-        delete valCouleurs[input.getAttribute("data-index")];
-        document.body.style.background = `linear-gradient(${inclinaison}deg, ${Object.values(
-          valCouleurs
-        )})`;
-        comment.textContent = "/* Dégradé linéaire */";
-        prop.textContent = "background";
-        ponct.textContent = ": ";
-        val.innerHTML = `<span class="linear">linear-gradient</span>(${inclinaison}deg, ${Object.values(
-          valCouleurs
-        )});`;
-      }
+  if (
+    Object.keys(valCouleurs).includes(input.getAttribute("data-index")) &&
+    Object.keys(valCouleurs).length < 3
+  ) {
+    delete valCouleurs[input.getAttribute("data-index")];
+    document.body.style.background = "#333";
+    comment.textContent =
+      "/* Il faut au moins deux couleurs pour générer un dégradé linéaire et son code CSS. */";
+    prop.textContent = "";
+    ponct.textContent = "";
+    val.innerHTML = "";
+  } else if (
+    Object.keys(valCouleurs).includes(input.getAttribute("data-index")) &&
+    Object.keys(valCouleurs).length > 2
+  ) {
+    delete valCouleurs[input.getAttribute("data-index")];
+    document.body.style.background = `linear-gradient(${inclinaison}deg, ${Object.values(
+      valCouleurs
+    )})`;
+    comment.textContent = "/* Dégradé linéaire */";
+    prop.textContent = "background";
+    ponct.textContent = ": ";
+    val.innerHTML = `<span class="linear">linear-gradient</span>(${inclinaison}deg, ${Object.values(
+      valCouleurs
+    )});`;
+  }
 }
 
-function ajoutCouleur(event) {
+function ajout_ModifCouleur(event) {
   if (/#[a-fA-F0-9]{6}/.test(event.target.value)) {
     event.target.previousElementSibling.style.background = event.target.value;
     valCouleurs[event.target.getAttribute("data-index")] = event.target.value;
@@ -110,23 +105,23 @@ function ajoutCouleur(event) {
   }
 }
 
-
 function suppInput(event) {
-    suppCouleur(event.target.parentNode.children[1]);
-    contCouleurs.removeChild(event.target.parentNode);
-  }
+  suppCouleur(event.target.parentNode.children[1]);
+  contCouleurs.removeChild(event.target.parentNode);
+  index--;
+}
 
 function randomCouleurs() {
   inputsCouleur = document.querySelectorAll(".inp-couleur");
   cerclesCouleur = document.querySelectorAll(".cercle-couleur");
   valCouleurs = {};
   const lettresChiffres = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
     0,
     1,
     2,
@@ -153,14 +148,14 @@ function randomCouleurs() {
 }
 
 function randomCouleur(event) {
-  const siblingNodes = event.target.parentNode.children; 
+  const siblingNodes = event.target.parentNode.children;
   const lettresChiffres = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
     0,
     1,
     2,
@@ -178,11 +173,9 @@ function randomCouleur(event) {
     randomCouleur += lettresChiffres[Math.floor(Math.random() * 16)];
   }
 
-    siblingNodes[0].style.background =
-    randomCouleur;
-    siblingNodes[1].value = randomCouleur;
-  valCouleurs[siblingNodes[1].getAttribute("data-index")] =
-    randomCouleur;
+  siblingNodes[0].style.background = randomCouleur;
+  siblingNodes[1].value = randomCouleur;
+  valCouleurs[siblingNodes[1].getAttribute("data-index")] = randomCouleur;
 
   verifNbCouleurs();
 }
@@ -196,18 +189,18 @@ inputRange.addEventListener("input", (e) => {
 
 // Bouton code CSS + bouton close
 
-[btnCssCode, btnClose].forEach(btn => {
-    btn.addEventListener("click", () => {
-        contCSS.classList.toggle("active");
-      });      
-})
+[btnCssCode, btnClose].forEach((btn) => {
+  btn.addEventListener("click", () => {
+    contCSS.classList.toggle("active");
+  });
+});
 
 // Bouton plus
 
 btnPlus.addEventListener("click", () => {
-    const inpGroup = document.createElement("div");
-    inpGroup.setAttribute("class", "inp-group");
-    inpGroup.innerHTML = `
+  const inpGroup = document.createElement("div");
+  inpGroup.setAttribute("class", "inp-group");
+  inpGroup.innerHTML = `
     <div class="cercle-couleur"></div>
     <input type="text" data-index="${index}" maxlength="7" class="inp-couleur">
     <button class="moins">
@@ -225,7 +218,7 @@ btnPlus.addEventListener("click", () => {
     </button>`;
   contCouleurs.appendChild(inpGroup);
   inpGroup.querySelector("input").addEventListener("input", (e) => {
-    ajoutCouleur(e);
+    ajout_ModifCouleur(e);
   });
   inpGroup.querySelector(".moins").addEventListener("click", (e) => {
     suppInput(e);
@@ -240,24 +233,34 @@ btnPlus.addEventListener("click", () => {
 // Bouton copier
 
 btnPaste.addEventListener("click", (e) => {
-    if(document.querySelector(".css").textContent !== "") {
-        navigator.clipboard.writeText(document.querySelector(".css").textContent).then(() => {
-        e.target.firstElementChild.innerHTML = '<path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/>';
-    setTimeout(() => { e.target.firstElementChild.innerHTML = '<path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/><path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>';
-}, 1000);
-      }).catch(() => {
-        e.target.firstElementChild.innerHTML = '<path d="M4.54.146A.5.5 0 0 1 4.893 0h6.214a.5.5 0 0 1 .353.146l4.394 4.394a.5.5 0 0 1 .146.353v6.214a.5.5 0 0 1-.146.353l-4.394 4.394a.5.5 0 0 1-.353.146H4.893a.5.5 0 0 1-.353-.146L.146 11.46A.5.5 0 0 1 0 11.107V4.893a.5.5 0 0 1 .146-.353L4.54.146zM5.1 1 1 5.1v5.8L5.1 15h5.8l4.1-4.1V5.1L10.9 1H5.1z"/><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>';
-    setTimeout(() => { e.target.firstElementChild.innerHTML = '<path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/><path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>';
-}, 1000);
+  if (document.querySelector(".css").textContent !== "") {
+    navigator.clipboard
+      .writeText(document.querySelector(".css").textContent)
+      .then(() => {
+        e.target.firstElementChild.innerHTML =
+          '<path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/>';
+        setTimeout(() => {
+          e.target.firstElementChild.innerHTML =
+            '<path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/><path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>';
+        }, 1000);
+      })
+      .catch(() => {
+        e.target.firstElementChild.innerHTML =
+          '<path d="M4.54.146A.5.5 0 0 1 4.893 0h6.214a.5.5 0 0 1 .353.146l4.394 4.394a.5.5 0 0 1 .146.353v6.214a.5.5 0 0 1-.146.353l-4.394 4.394a.5.5 0 0 1-.353.146H4.893a.5.5 0 0 1-.353-.146L.146 11.46A.5.5 0 0 1 0 11.107V4.893a.5.5 0 0 1 .146-.353L4.54.146zM5.1 1 1 5.1v5.8L5.1 15h5.8l4.1-4.1V5.1L10.9 1H5.1z"/><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>';
+        setTimeout(() => {
+          e.target.firstElementChild.innerHTML =
+            '<path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/><path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>';
+        }, 1000);
       });
-    } else {
-        e.target.firstElementChild.innerHTML = '<path d="M4.54.146A.5.5 0 0 1 4.893 0h6.214a.5.5 0 0 1 .353.146l4.394 4.394a.5.5 0 0 1 .146.353v6.214a.5.5 0 0 1-.146.353l-4.394 4.394a.5.5 0 0 1-.353.146H4.893a.5.5 0 0 1-.353-.146L.146 11.46A.5.5 0 0 1 0 11.107V4.893a.5.5 0 0 1 .146-.353L4.54.146zM5.1 1 1 5.1v5.8L5.1 15h5.8l4.1-4.1V5.1L10.9 1H5.1z"/><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>';
-    setTimeout(() => { e.target.firstElementChild.innerHTML = '<path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/><path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>';
-}, 1000);
-    }
-
+  } else {
+    e.target.firstElementChild.innerHTML =
+      '<path d="M4.54.146A.5.5 0 0 1 4.893 0h6.214a.5.5 0 0 1 .353.146l4.394 4.394a.5.5 0 0 1 .146.353v6.214a.5.5 0 0 1-.146.353l-4.394 4.394a.5.5 0 0 1-.353.146H4.893a.5.5 0 0 1-.353-.146L.146 11.46A.5.5 0 0 1 0 11.107V4.893a.5.5 0 0 1 .146-.353L4.54.146zM5.1 1 1 5.1v5.8L5.1 15h5.8l4.1-4.1V5.1L10.9 1H5.1z"/><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>';
+    setTimeout(() => {
+      e.target.firstElementChild.innerHTML =
+        '<path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/><path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>';
+    }, 1000);
+  }
 });
-
 
 // Boutons random pour changer la valeur d'un seul input
 
@@ -267,6 +270,6 @@ btnsRandom1.forEach((btnRandom) =>
   })
 );
 
-// Bouton random pour changer la valeur de tous les inputs 
+// Bouton random pour changer la valeur de tous les inputs
 
 btnRandom2.addEventListener("click", randomCouleurs);
